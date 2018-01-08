ef6e0bdb4b46308f91f626afc9e86123a2668b92
Akoni <hakmorrison@csumb.edu>
2018-01-08 08:17:24 +0000

    added mongolab

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes       = require("./routes/comments"),
     indexRoutes          = require("./routes/index");
 
 
-mongoose.connect("mongodb://localhost/yelp_camp_v12", {useMongoClient: true});
+//mongoose.connect("mongodb://localhost/yelp_camp_v12", {useMongoClient: true});
+mongoose.connect("mongodb://akoni:maxsan@ds245277.mlab.com:45277/yelpcamp", {useMongoClient: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
